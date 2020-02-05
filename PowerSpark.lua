a4f0da8f4412ece6aaad72a90a7b52e70fe6f44d
starpt <starpt@gmail.com>
2020-02-05 16:26:03 +0800

    更新到 1.2.5版本

--- PowerSpark.lua
+++ PowerSpark.lua
@@ -1,114 +1,88 @@
-if select(2, UnitClass('player')) == 'WARRIOR' then return end
-local PowerSparkDB = {
-	default = {
-		name = 'PowerSparkFrameManaBar',
-		parent = PlayerFrameManaBar
-	},
-	druid = {
-		name = 'PowerSparkFrameDruidManaBar',
-		parent = DruidBarFrame,
-		enable = select(2, UnitClass('player')) == 'DRUID' and DruidBarFrame and DruidBarKey --小德蓝条启用条件
-	}
-}
-local PowerSparkFrame = CreateFrame('Frame')
-PowerSparkFrame:RegisterEvent('PLAYER_ENTERING_WORLD')
-PowerSparkFrame:RegisterEvent('UNIT_POWER_UPDATE', 'player')
-PowerSparkFrame:SetScript('OnEvent', function(self, event)
+local class = select(2, UnitClass('player'))
+local frame = CreateFrame('Frame')
+for _, v in pairs({'PLAYER_ENTERING_WORLD', 'UNIT_SPELLCAST_SUCCEEDED', 'UNIT_MAXPOWER', 'UNIT_POWER_FREQUENT'}) do
+	frame:RegisterEvent(v, 'player')
+end
+if class == 'ROGUE' then frame:RegisterEvent('UNIT_AURA') end
+frame:SetScript('OnEvent', function(self, event, ...)
 	if event == 'PLAYER_ENTERING_WORLD' then
-		local last
-		if select(1, UnitPowerType('player')) == 3 then
-			last = UnitPower('player')
-		else
-			last = UnitPower('player', 0)
+		function self.cure(key)
+			local type = UnitPowerType('player')
+			local cure = UnitPower('player', type)
+			if key == 'druid' then
+				type = 0
+				cure = DruidBarKey.currentmana
+			end
+			return cure, type
 		end
-		PowerSparkFrame:init(PowerSparkDB.default, last) --默认界面初始化
-		if PowerSparkDB.druid.enable then PowerSparkFrame:init(PowerSparkDB.druid, DruidBarKey.currentmana) end --小德界面初始化
-	end
-	if event == 'UNIT_POWER_UPDATE' then
-		local powerType = select(1, UnitPowerType('player'))
-		if powerType == 3 then
-			PowerSparkFrame:energy(PowerSparkDB.default)
-		elseif powerType == 0 then
-			if not ChannelInfo() then PowerSparkFrame:mana(PowerSparkDB.default, UnitPower('player', 0)) end
+		function self.wait(key)
+			if UnitPowerType('player') == 0 or key == 'druid' then self[key].wait = GetTime() + 5 end
 		end
-		if PowerSparkDB.druid.enable and not ChannelInfo() then
-			PowerSparkFrame:mana(PowerSparkDB.druid, DruidBarKey.currentmana)
+		function self.rest(key)
+			local cure, type = self.cure(key)
+			if type == 3 then self[key].wait = nil end
+			if cure > self[key].cure then self[key].timer = GetTime() end
+			self[key].cure = cure
 		end
-	end
-end)
-PowerSparkFrame:SetScript('OnUpdate', function()
-	PowerSparkFrame:flash(PowerSparkDB.default)
-	if PowerSparkDB.druid.enable then PowerSparkFrame:flash(PowerSparkDB.druid) end
-end)
-
-function PowerSparkFrame:init(power, last)
-	if power.bar then return end
-	power.bar = CreateFrame('Statusbar', power.name, power.parent)
-	power.bar:SetWidth(PlayerFrameManaBar:GetWidth() - 2)
-	power.bar:SetHeight(power.parent:GetHeight() - 2)
-	power.bar:SetPoint('CENTER')
-	power.spark = power.bar:CreateTexture(nil, 'OVERLAY')
-	power.spark:SetTexture('Interface\\CastingBar\\UI-CastingBar-Spark')
-	power.spark:SetWidth(32)
-	power.spark:SetHeight(32)
-	power.spark:SetBlendMode('ADD')
-	power.spark:SetAlpha(0)
-	power.timer = power.timer or GetTime() --定时器
-	power.interval = power.interval or 2
-	power.last = last
-end
-
-function PowerSparkFrame:energy(power) --回能量
-	if UnitPower('player') > power.last and UnitPower('player') <= power.last + 20 then --不触发其它加能量方法
-		power.timer = GetTime()
-		power.interval = 2
-	end
-	power.last = UnitPower('player')
-end
-
-function PowerSparkFrame:mana(power, mp) --回蓝
-	if mp < power.last then
-		power.timer = GetTime()
-		power.interval = 5
-	elseif GetTime() >= power.timer + power.interval then
-		power.timer = GetTime()
-		power.interval = 2
-	end
-	power.last = mp
-end
-
-function PowerSparkFrame:flash(power) --闪动
-	local powerType = select(1, UnitPowerType('player'))
-	if UnitIsDeadOrGhost('player') then
-		power.bar:Hide()
-	elseif powerType == 1 and not power.enable then  --熊的怒气槽
-		power.bar:Hide()
-	elseif powerType == 0 and UnitPower('player', 0) >= UnitPowerMax('player', 0) or power.enable and DruidBarKey.currentmana >= UnitPowerMax('player', 0) then --蓝条满
-		power.bar:Hide()
-	elseif powerType == 3 and not power.enable and UnitPower('player') >= UnitPowerMax('player') then
-		if UnitCanAttack('player', 'target') and not UnitIsDeadOrGhost('target') then --可攻击目标
-			power.bar:Show()
-		else
-			power.bar:Hide()
-		end
-	else
-		power.bar:Show()
-	end
-	if not power.bar:IsVisible() then return end
-
-	if power.interval > 2 then --5秒等待回蓝
-		if power.timer + power.interval > GetTime() then
-			power.spark:SetAlpha(.75)
-			power.spark:SetPoint('CENTER', power.bar, 'LEFT', power.bar:GetWidth() - power.bar:GetWidth() * (mod(GetTime() - power.timer, power.interval) / power.interval), 0)
-		else --5秒后等待恢复
+		function self.init(parent, key) --初始化
+			if not parent then return end
+			local now = GetTime()
+			local type = UnitPowerType('player')
+			local power = CreateFrame('StatusBar', nil, parent)
+			--print(parent:GetWidth())
+			power:SetWidth(PlayerFrameManaBar:GetWidth())
+			power:SetHeight(parent:GetHeight())
+			power:SetPoint('CENTER')
+			power.spark = power:CreateTexture(nil, 'OVERLAY')
+			power.spark:SetTexture('Interface\\CastingBar\\UI-CastingBar-Spark')
+			power.spark:SetWidth(32)
+			power.spark:SetHeight(32)
+			power.spark:SetBlendMode('ADD')
 			power.spark:SetAlpha(0)
+			power.rate = now
+			power.cure = UnitPower('player', type)
+			power.timer = now
+			if key == 'DRUID' then power.cure = DruidBarKey.currentmana end
+			power.interval = 2
+			power.key = key
+			function power.hide(key)
+				local cure, type = self.cure(key)
+				return UnitIsDeadOrGhost('player') or key == 'default' and type == 1 or type == 0 and cure >= UnitPowerMax('player', 0) or type == 3 and cure >= UnitPowerMax('player') and not IsStealthed() and (not UnitCanAttack('player', 'target') or UnitIsDeadOrGhost('target')) --角色死亡/怒气/满蓝/满能量且不潜行且目标不可攻击
+			end
+			power:HookScript('OnUpdate', function(self)
+				local now = GetTime()
+				if now < self.rate then return end
+				self.rate = now + 0.03 --刷新率
+				if self.hide(self.key) then
+					self.spark:SetAlpha(0)
+					return
+				end
+				self.spark:SetAlpha(1)
+				if self.wait and self.wait > now then --5秒等待回蓝
+					self.spark:SetPoint('CENTER', self, 'LEFT', self:GetWidth() * (self.wait - now) / 5, 0)
+				else
+					self.spark:SetPoint('CENTER', self, 'LEFT', self:GetWidth() * (mod(now - self.timer, self.interval) / self.interval), 0)
+				end
+			end)
+			self[key] = power
 		end
-	else
-		power.spark:SetAlpha(.5)
-		if power.timer + power.interval > GetTime() then
-			power.spark:SetPoint('CENTER', power.bar, 'LEFT', power.bar:GetWidth() * (mod(GetTime() - power.timer, power.interval) / power.interval), 0)
-		else --满能量
-			power.spark:SetPoint('CENTER', power.bar, 'LEFT', power.bar:GetWidth() * (mod(math.fmod(GetTime() - power.timer, 2), 2) / 2), 0)
+		self.init(PlayerFrameManaBar, 'default')
+		if class == 'DRUID' and DruidBarFrame and DruidBarKey then self.init(DruidBarFrame, 'druid') end
+	elseif event == 'UNIT_SPELLCAST_SUCCEEDED' then
+		self.wait('default')
+		if self.druid then self.wait('druid') end
+	elseif event =='UNIT_MAXPOWER' or event == 'UNIT_POWER_FREQUENT' then
+		self.rest('default')
+		if self.druid then self.rest('druid') end
+	elseif event == 'UNIT_AURA' then
+		self.interval = 2
+		local i = 1
+		while UnitBuff('player', i) do
+			if select(10,UnitBuff('player', i)) == 13750 then --开了冲动
+				self.interval = 1
+				break
+			end
+			i = i + 1
 		end
 	end
-end
+end)
